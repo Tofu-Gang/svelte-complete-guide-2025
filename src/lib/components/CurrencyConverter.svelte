66919f9986a9a78efb3a1c884f5f6d99ba2a3ee0
Jakub Franěk <jfranek@hstechnology.cz>
2025-03-14 11:10:47 +0100

    47. Currency Converter Component Initialization

---
+++ src/lib/components/CurrencyConverter.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+	import dummyRates from '$lib/utils/dummy-rates';
+
+	let baseValue = $state(1);
+	let baseCurrency = $state("usd");
+	let baseRates = $derived(dummyRates[baseCurrency]);
+	let targetCurrency = $state("eur");
+	let targetValue = $derived(baseValue && baseRates[targetCurrency] && baseValue * baseRates[targetCurrency]);
+</script>
+
+<div class="wrapper">
+	<div class="conversion">
+		<span class="base">{Number(1).toLocaleString('en-US', {
+			style: 'currency',
+			currency: baseCurrency,
+			currencyDisplay: 'name'
+		})} equals
+		</span>
+		<span class="target">{baseRates[targetCurrency].toLocaleString('en-US', {
+			style: 'currency',
+			currency: targetCurrency,
+			currencyDisplay: 'name'
+		})}
+		</span>
+	</div>
+	<div class="base">
+		<!-- specify getter and setter functions for bind:value directive -->
+		<input type="number" bind:value={
+		() => baseValue,
+		(newValue) => {
+			if(newValue < 0) {
+				baseValue = 1;
+				return;
+			}
+			baseValue = newValue
+		}}>
+		<select bind:value={baseCurrency}>
+			<option value="usd">United States Dollar</option>
+			<option value="eur">Euro</option>
+			<option value="gbp">Pound Sterling</option>
+		</select>
+	</div>
+	<div class="target">
+		<input value={targetValue} type="number">
+		<select bind:value={targetCurrency}>
+			<option value="usd">United States Dollar</option>
+			<option value="eur">Euro</option>
+			<option value="gbp">Pound Sterling</option>
+		</select>
+	</div>
+</div>
+
+<style lang="scss">
+  .wrapper {
+    font-family: Arial, Helvetica, sans-serif;
+    background-color: #131313;
+    padding: 20px;
+    margin: 20px 10px;
+    border-radius: 10px;
+    .conversion {
+      margin-bottom: 20px;
+      span.base {
+        opacity: 0.6;
+        font-size: 14px;
+        display: block;
+        margin-bottom: 5px;
+      }
+      span.target {
+        font-size: 28px;
+        display: block;
+      }
+    }
+    .base {
+      margin-bottom: 15px;
+    }
+    .base,
+    .target {
+      select,
+      input {
+        background-color: transparent;
+        color: #fff;
+        border: 1px solid rgba(255, 255, 255, 0.1);
+        border-radius: 5px;
+        padding: 10px;
+        &:focus-visible {
+          outline: 1px solid rgb(65, 189, 209);
+        }
+      }
+      input {
+        &::-webkit-outer-spin-button,
+        &::-webkit-inner-spin-button {
+          -webkit-appearance: none;
+          margin: 0;
+        }
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,8 @@
 <script lang="ts">
-	// import Proxy from '$lib/components/Proxy.svelte';
-	// import DeepState from '$lib/components/DeepState.svelte';
-	import Notifications from '$lib/components/Notifications.svelte';
-	import Sheet from '$lib/components/sheet/Sheet.svelte';
-	import { test_data } from '$lib/components/sheet/utils';
-
-	let data = $state(test_data);
+import CurrencyConverter from '$lib/components/CurrencyConverter.svelte';
 </script>
 
-<hr>
-<p>33. Javascript Proxies and Object Setters & Getters</p>
-<hr>
-<!--<Proxy />-->
-<hr>
-<p>34. Deep State Reactivity in Svelte</p>
-<hr>
-<p>35. Debugging in Svelte</p>
-<hr>
-<!--<DeepState />-->
-<hr>
-<p>36. Displaying a List of Data (#each loops & @const tags)</p>
-<hr>
-<p>37. Handling Removing Elements from An Array Correctly</p>
-<hr>
-<p>38. Immutable Raw State</p>
-<hr>
-<Notifications />
-<hr>
-<p>39. Spreadsheet Component: Initialization & Helper Functions</p>
-<hr>
-<p>40. Spreadsheet Component: Rendering a Table with Cells and Values</p>
-<hr>
-<p>41. Spreadsheet Component: Selecting & Editing Cells</p>
-<hr>
-<p>42. Spreadsheet Component: Updating Cells Values</p>
-<hr>
-<p>43. Spreadsheet Component: Handling SUM & MULTIPLY Functions</p>
-<hr>
-<p>44. Spreadsheet Component: Binding the Data Prop with $bindable()</p>
-<hr>
-<p>45. Spreadsheet Component: Changing the Cell Background and Font Colours</p>
-<hr>
-<pre>{JSON.stringify(data, null, 2)}</pre>
-<!-- allow Sheet component to mutate the data by using bind: -->
-<!-- since prop "data" is defined as optional, we can omit bind:data entirely and default value
-(from $bindable rune in Sheet component) will be used -->
-<Sheet bind:data />
+<CurrencyConverter />
 
 <style>
 	:global(body) {
