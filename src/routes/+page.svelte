29ba0ef867c07fbd565c47b9971bf2307e621aa3
Jakub Franěk <jfranek@hstechnology.cz>
2025-03-19 16:14:23 +0100

    59. Introduction to Universal Reactivity

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,10 @@
 <script lang="ts">
-	import { SvelteDate, SvelteURL } from 'svelte/reactivity';
-	import CurrencyConverter from '$lib/components/CurrencyConverter.svelte';
-
-	let date = new SvelteDate();
-	const url = new SvelteURL("https://example.com/path");
-
-	$effect(() => {
-		const interval = setInterval(() => {
-			date.setTime(Date.now());
-		}, 1000);
-
-		return () => {
-			clearInterval(interval);
-		}
-	})
+import ClickToCount from '$lib/components/ClickToCount.svelte';
+import DoubleClickToCount from '$lib/components/DoubleClickToCount.svelte';
 </script>
 
-<CurrencyConverter />
-
-<p>
-	{date.getHours().toString().padStart(2, "0")}:{date.getMinutes().toString().padStart(2, "0")}:{date.getSeconds().toString().padStart(2, "0")}
-</p>
-
-<!-- changes to these -->
-<input bind:value={url.protocol} />
-<input bind:value={url.hostname} />
-<input bind:value={url.pathname} />
-<hr />
-<!-- will update `href` and vice versa -->
-<input bind:value={url.href} />
+<ClickToCount />
+<DoubleClickToCount />
 
 <style>
 	:global(body) {
