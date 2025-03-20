eaeee85499f09b38615593912c57210f93f974e3
Jakub Franěk <jfranek@hstechnology.cz>
2025-03-20 16:22:49 +0100

    61. Extracting Logic with Classes

--- src/lib/utils/counter.svelte.ts
+++ src/lib/utils/counter.svelte.ts
@@ -1,15 +1,19 @@
-function createCounter() {
-	let count: number = $state(0);
-	$effect(() => {
-		console.log(count);
-	});
-	return {
-		get value(): number {
-			return count;
-		},
-		increment: (): number => count++,
-		reset: (): number => (count = 0)
+class Counter {
+	value: number = $state(0);
+
+	constructor() {
+		$effect(() => {
+			console.log(this.value);
+		});
+	}
+
+	increment = () => {
+		this.value++;
+	};
+
+	reset = () => {
+		this.value = 0;
 	};
 }
 
-export default createCounter;
+export default Counter;
