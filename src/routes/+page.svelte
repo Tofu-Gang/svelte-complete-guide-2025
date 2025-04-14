e2079d0bf4161338b5779b40f603e8eee79ed445
Jakub Franěk <jfranek@hstechnology.cz>
2025-04-14 17:08:16 +0200

    9. Svelte Component Basics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import DisplayName from '$lib/components/section-1/DisplayName.svelte';
+</script>
+
+<h1>Outer</h1>
+<DisplayName />
