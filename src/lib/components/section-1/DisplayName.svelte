b396c514f2559bfb2a1fab85b397c303bb48e5ab
Jakub Franěk <jfranek@hstechnology.cz>
2025-04-14 17:38:17 +0200

    10. Adding Reactivity to Our Component

--- src/lib/components/section-1/DisplayName.svelte
+++ src/lib/components/section-1/DisplayName.svelte
@@ -1,11 +1,39 @@
 <script lang="ts">
-	let name = "Jakub Franěk";
+	let firstName = $state("Jakub");
+	let lastName = $state("Franěk");
+	let fullName = $derived.by(() => {
+		return `${firstName} ${lastName}`;
+	});
+	let userName = $state("jfranek");
 	let src = "https://picsum.photos/250/300";
 	let bio = "Hello, <b>this</b> is me.";
+
+	$effect(() => {
+		if(userName || fullName) {
+			console.log("User has a user name or a full name.");
+		}
+	});
 </script>
 
-<img {src} alt="A photo of {name}">
-<h1>{name}</h1>
+<ul>
+	<li>
+		<label for="userName">User Name</label>
+		<input bind:value={userName} name="userName" />
+	</li>
+
+	<li>
+		<label for="firstName">First Name</label>
+		<input bind:value={firstName} name="firstName" />
+	</li>
+
+	<li>
+		<label for="lastName">Last Name</label>
+		<input bind:value={lastName} name="lastName" />
+	</li>
+</ul>
+
+<img {src} alt="A photo of {fullName}">
+<h1>{userName || fullName}</h1>
 <p>{@html bio}</p>
 
 <style>
